cb6def728e105d99a939a18b7a188bdf84d23d4e
mnezmah <mario.nezmah@gmail.com>
2019-01-15 18:49:11 +0100

    webpack config for production

--- webpack.config.js
+++ webpack.config.js
@@ -1,38 +1,40 @@
 // entry -> output
 const path = require('path');
-
-module.exports = {
-  entry: './src/app.js',
-  output: {
-    path: path.join(__dirname, 'public'),
-    filename: 'bundle.js'
-  },
+module.exports = (env) =>  {
+  const isProduction = env === 'production';
+  console.log('env, env')
+  return {
+    entry: './src/app.js',
+    output: {
+      path: path.join(__dirname, 'public'),
+      filename: 'bundle.js'
+    },
   // saying: whenewer you see and js file, run it through our app ,but exclude node_modules
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use:{
-          loader: 'babel-loader',
-          options:{ presets:['env','react'], 
-                    plugins:['transform-class-properties']
-                  } 
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          exclude: /node_modules/,
+          use:{
+            loader: 'babel-loader',
+            options:{ presets:['env','react'], 
+                      plugins:['transform-class-properties']
+                    } 
+          }
+        },
+        {
+          test: /\.s?css$/,
+          use: [
+            'style-loader',
+            'css-loader',
+           'sass-loader'
+         ]
         }
-      },
-      {
-        test: /\.s?css$/,
-        use: [
-          'style-loader',
-          'css-loader',
-          'sass-loader'
-        ]
-      }
-    ]
-  },
-  devtool: 'cheap-module-source-map',
-  devServer: {
-    contentBase: path.join(__dirname, 'public')
-  }
+      ]
+   },
+   devtool: isProduction ? 'source-map' : 'cheap-module-source-map',
+    devServer: {
+      contentBase: path.join(__dirname, 'public')
+    }
+  };
 };
-
